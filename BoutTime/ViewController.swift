407d18b7708754af4cfc974ea5a7b3b3f8cca06c
tenred <sheriefwissa@magneto-3.local>
2016-11-14 12:14:42 +1100

    Initial Commit

---
+++ BoutTime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BoutTime
+//
+//  Created by Sherief Wissa on 14/11/16.
+//  Copyright © 2016 10 Red Hacks Pty Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
