b74aaee2e5096011ff86204494e324669432053b
tenred <sheriefwissa@magneto-3.local>
2016-11-15 11:37:51 +1100

    - Implementation of UI, Splash & Constrains. - Implementation of pList Datafile. - Implementation of Shake Action Response.

---
+++ BoutTime/BoutTimeGame.swift
@@ -0,0 +1,11 @@
+//
+//  BoutTimeGame.swift
+//  BoutTime
+//
+//  Created by Sherief Wissa on 15/11/16.
+//  Copyright © 2016 10 Red Hacks Pty Ltd. All rights reserved.
+//
+
+import Foundation
+
+
--- BoutTime/ViewController.swift
+++ BoutTime/ViewController.swift
@@ -11,20 +11,36 @@ import UIKit
 class ViewController: UIViewController {
 
     //
+    @IBOutlet var eventLabelCollection: [UILabel]!
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // ViewController to become first responder to shake event.
+        self.becomeFirstResponder()
+        
+        UIStateFirstLoad()
     }
-
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        //Override function to respond to shake action.
+        
+        print("Shakey Shakey")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
-    func hello(){
-        
-    }
     
+    func UIStateFirstLoad() {
+    // Function loads the default UI state with app is first launched.
+        
+        for label in eventLabelCollection {
+            label.layer.masksToBounds = true
+            label.layoutMargins = UIEdgeInsetsMake(-20, -20,0,0)
+        }
+    }
 }
 
